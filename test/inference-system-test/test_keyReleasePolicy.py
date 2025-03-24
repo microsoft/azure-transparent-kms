f2e8a72adfa9a167225c80954739a5a91e9d51da
smore <simore@microsoft.com>
2025-03-24 22:27:18 +0000

    Add Test without operator on KeyReleaseClaims

--- test/inference-system-test/test_keyReleasePolicy.py
+++ test/inference-system-test/test_keyReleasePolicy.py
@@ -1,6 +1,37 @@
 import pytest
 from endpoints import setKeyReleaseClaims, keyReleasePolicy
 
+def test_keyReleaseClaim_add_remove(setup_kms):
+    # Add claims by calling SetKeyRelease Endpoints
+
+    status_code, key_release_json = setKeyReleaseClaims(
+        type="add",
+        claims={
+            "claims": {
+                "x-ms-ver": ["1.0"],
+                "x-ms-azurevm-debuggersdisabled": True,
+                "x-ms-azurevm-osversion-major": [22, 23]
+            }
+        }
+    )
+    assert status_code == 200
+    status_code, key_release_json = keyReleasePolicy()
+    assert status_code == 200
+    assert key_release_json["claims"]["x-ms-ver"] == ["1.0"]
+    assert key_release_json["claims"]["x-ms-azurevm-debuggersdisabled"] == True
+    assert key_release_json["claims"]["x-ms-azurevm-osversion-major"] == [22, 23]
+
+    # Remove Claims
+    status_code, key_release_json = setKeyReleaseClaims(
+        type="remove",
+        claims={
+            "claims": {
+                "x-ms-attestation-type": "test-value"
+            }
+        }
+    )
+    assert status_code == 200
+
 
 def test_keyReleaseClaim_add_remove_with_operators(setup_kms):
     # Add claims by calling SetKeyRelease Endpoints

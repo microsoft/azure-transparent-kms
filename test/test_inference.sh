ef295ba1cfe239702da7c90d16a8904b3a16101f
smore <simore@microsoft.com>
2025-03-31 22:11:15 +0000

    Add JWT and KeyRelease Policy before testing

--- test/test_inference.sh
+++ test/test_inference.sh
@@ -80,6 +80,60 @@ sudo chmod +r ${certificate_dir}/*.pem
 export SERVER=${server}
 export CERTS_FOLDER=${certificate_dir}
 export INTERACTIVE_MODE=${interactive}
+
+
+echo "Setting JWT validation policy before running e2e test..."
+
+
+issuer="http://Demo-jwt-issuer"
+response=$(curl -sS "$server/app/setJwtValidationPolicy" \
+    --cacert "/workspaces/azure-transparent-kms/workspace/sandbox_common/service_cert.pem" \
+    --cert "/workspaces/azure-transparent-kms/workspace/sandbox_common/user0_cert.pem" \
+    --key "/workspaces/azure-transparent-kms/workspace/sandbox_common/user0_privk.pem" \
+    -H "Content-Type: application/json" \
+    -d "{\"jwt_validation_policy\": {\"issuer\": \"http://Demo-jwt-issuer\", \"validation_policy\": {\"iss\": \"http://Demo-jwt-issuer\"}}}" \
+    -w '\n%{http_code}')
+
+# Split status and body
+status_code=$(echo "$response" | tail -n1)
+body=$(echo "$response" | head -n -1)
+
+echo "JWT Policy Response Body: $body"
+echo "Status Code : $status_code"
+
+if [ "$status_code" -ne 200 ]; then
+  echo "Failed to set JWT validation policy"
+  echo "Response: $body"
+  exit 1
+fi
+
+
+claims='{
+  "claims": {
+    "x-ms-ver": ["1.0"],
+    "x-ms-azurevm-debuggersdisabled": true,
+    "x-ms-azurevm-osversion-major": [22],
+    "x-ms-azurevm-os-provisioning.node-policy-identity.eventVersion": 1,
+    "x-ms-azurevm-os-provisioning.node-policy-identity.policyId": "openai-whisper",
+    "x-ms-azurevm-os-provisioning.node-policy-identity.signer": "8fe6e7a314b8695b21710cebf0265e8d7bbaabde26f431c407faf16fcbd6b924", # pragma: allowlist secret
+    "x-ms-azurevm-os-provisioning.os-image-identity.diskId": "singularity.ubuntu-22.04",
+    "x-ms-azurevm-os-provisioning.os-image-identity.eventVersion": 1,
+    "x-ms-azurevm-os-provisioning.os-image-identity.signer": "f9cce5b7bdc2aaacfc4c78cb2b7515459aded8149287b74667bb2f178b0cf7b9" # pragma: allowlist secret
+  }
+}'
+
+type="add"
+
+response=$(curl -s "$server/app/setKeyReleasePolicyClaims" \
+  --cacert "/workspaces/azure-transparent-kms/workspace/sandbox_common/service_cert.pem" \
+  --cert "/workspaces/azure-transparent-kms/workspace/sandbox_common/user0_cert.pem" \
+  --key "/workspaces/azure-transparent-kms/workspace/sandbox_common/user0_privk.pem" \
+  -H "Content-Type: application/json" \
+  -d "{\"claimType\": \"$type\", \"keyReleaseClaims\": $claims}" \
+  -w '\n%{http_code}\n')
+
+echo "$response"
+
 npm run e2e-inference-test
 
 printf "\n\n🏁 Test Completed...\n"
